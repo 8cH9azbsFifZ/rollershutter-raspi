120faaa1103bdb2ec95fc24afdfdb7a16981511a
8cH9azbsFifZ <g@ziegenhain.com>
2021-07-13 19:18:56 +0200

    initial

---
+++ README.md
@@ -0,0 +1,6 @@
+#Rollershutter-Raspi
+
+Raspi for controlling a rollershutter for integration with
+OpenHAB using a MQTT binding.
+
+
---
+++ rollershutter.py
@@ -0,0 +1,71 @@
+import time
+import RPi.GPIO as GPIO
+import paho.mqtt.client as mqtt
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='Rollershutter(%(threadName)-10s) %(message)s')
+
+class Rollershutter():
+    def __init__(self, TimeUpwards = 10. , TimeDownwards = 10., MQTThostname = "t20", RollershutterName="Test1"):
+        self.Name = RollershutterName
+
+        GPIO.setmode(GPIO.BCM)
+
+        # Connect to MQTT broker
+        port = 1883
+        self._client = mqtt.Client()
+        self._client.connect(MQTThostname, port, 60)
+        self._client.on_connect = self._on_connect
+        self._client.on_message = self._on_message
+
+        self._samplingrate = 0.01
+
+    def _on_connect(self, client, userdata, flags, rc):
+        """ Connect to MQTT broker and subscribe to control messages """
+        logging.debug("Connected with result code " + str(rc))
+        self._client.subscribe("rollershutter/control/" + self.Name)
+
+    def _sendmessage(self, topic="/none", message="None"):
+        """ Send a message using MQTT """
+        ttopic = "rollershutter/" + self.Name + "/" + topic
+        mmessage = str(message)
+        logging.debug("MQTT>: " + ttopic + " ###> " + mmessage)
+        self._client.publish(ttopic, mmessage)
+
+    def _on_message(self, client, userdata, msg):
+        """
+        Receive MQTT control messages.
+        Start with debugging on commandline using:
+        mosquitto_pub -h localhost -t hangboard/workout/control -m Start
+        """
+        logging.debug(">MQTT: " + msg.payload.decode())
+        if msg.payload.decode() == "Stop":
+            self.Stop()
+        if msg.payload.decode() == "Up":
+            self.Up()
+        if msg.payload.decode() == "Down":
+            self.Down()
+        if msg.payload.decode() == "Percent": # FIXME
+            self.Percent()
+
+    def Down(self):
+        pass
+
+    def Up(self):
+        pass
+
+    def Stop(self):
+        pass
+
+    def Percent(self, percentage):
+        pass
+
+    def _core_loop(self):
+        while True:
+            self._client.loop(self._samplingrate) #blocks for 100ms (or whatever variable given, default 1s)
+
+
+if __name__ == "__main__":
+    print ("Run manual")
+    r = Rollershutter()
+    r._core_loop()
